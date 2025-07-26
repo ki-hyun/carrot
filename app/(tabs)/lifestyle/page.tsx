9616a0ee932f0ba91c17cd3e651bc50d5e198774
ki-hyun-kim <orgajja@gmail.com>
2025-07-27 01:02:05 +0900

    like00

--- app/(tabs)/lifestyle/page.tsx
+++ app/(tabs)/lifestyle/page.tsx
@@ -1,9 +1,71 @@
-export default function lifestyle(){
+import db from "@/lib/db";
+import { formatToTimeAgo } from "@/lib/utils";
+import {
+  ChatBubbleBottomCenterIcon,
+  HandThumbUpIcon,
+} from "@heroicons/react/24/outline";
+import Link from "next/link";
+
+export const dynamic = "force-dynamic";
+
+async function getPosts() {
+  const posts = await db.post.findMany({
+    select: {
+      id: true,
+      title: true,
+      description: true,
+      views: true,
+      created_at: true,
+      _count: {
+        select: {
+          comments: true,
+          likes: true,
+        },
+      },
+    },
+  });
+  return posts;
+}
+
+export const metadata = {
+  title: "동네생활",
+};
+
+export default async function Life() {
+
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  
+  const posts = await getPosts();
   return (
-    <div>
-      <h1 className="text-white text-4xl">
-        Life!
-      </h1>
+    <div className="p-5 flex flex-col">
+      <h4>Life </h4>
+      {posts.map((post) => (
+        <Link
+          key={post.id}
+          href={`/posts/${post.id}`}
+          className="pb-5 mb-5 border-b border-neutral-500 text-neutral-400 flex  flex-col gap-2 last:pb-0 last:border-b-0"
+        >
+          <h2 className="text-white text-lg font-semibold">{post.title}</h2>
+          <p>{post.description}</p>
+          <div className="flex items-center justify-between text-sm">
+            <div className="flex gap-4 items-center">
+              <span>{formatToTimeAgo(post.created_at.toString())}</span>
+              <span>·</span>
+              <span>조회 {post.views}</span>
+            </div>
+            <div className="flex gap-4 items-center *:flex *:gap-1 *:items-center">
+              <span>
+                <HandThumbUpIcon className="size-4" />
+                {post._count.likes}
+              </span>
+              <span>
+                <ChatBubbleBottomCenterIcon className="size-4" />
+                {post._count.comments}
+              </span>
+            </div>
+          </div>
+        </Link>
+      ))}
     </div>
-  )
+  );
 }
